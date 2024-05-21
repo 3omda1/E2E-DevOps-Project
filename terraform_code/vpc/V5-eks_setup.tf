c17878c604a4e3b0a62f1e069a0f4e77557febf5
3omda1 <ahmedemad97531@gmail.com>
2024-05-21 03:42:11 +0300

    added deployment manifest files

--- terraform_code/vpc/V5-eks_setup.tf
+++ terraform_code/vpc/V5-eks_setup.tf
@@ -103,14 +103,14 @@ resource "aws_route_table_association" "dpp-rta-public-subnet-02" {
   route_table_id = aws_route_table.dpp-public-rt.id   
 }
 
-  module "sgs" {
-    source = "../sg_eks"
-    vpc_id     =     aws_vpc.dpp-vpc.id
-  }
+module "sgs" {
+  source = "../sg_eks"
+  vpc_id     =     aws_vpc.dpp-vpc.id
+}
 
-  module "eks" {
-      source = "../eks"
-       vpc_id     =     aws_vpc.dpp-vpc.id
-       subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
-      sg_ids = module.sgs.security_group_public
-  }
+module "eks" {
+    source = "../eks"
+    vpc_id     =     aws_vpc.dpp-vpc.id
+    subnet_ids = [aws_subnet.dpp-public-subnet-01.id,aws_subnet.dpp-public-subnet-02.id]
+    sg_ids = module.sgs.security_group_public
+}
